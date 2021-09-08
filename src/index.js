2c59bd3ee171068d5843a292efc0236010d7503e
YuraKolodiy13 <timber2017@gmail.com>
2021-09-08 18:13:46 +0300

    add css, scss support, add fonts and images support

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from "react"
 import ReactDOM from "react-dom"
-import './index.css';
+import './index.scss';
+import excelIcon from './assets/images/excel.svg'
+import csvFile from './assets/files/export.csv';
 
 const App = () => (
-  <div className='app'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias deserunt ea nihil, nobis obcaecati perferendis sit tempore! Aperiam cupiditate eligendi fugit neque! Aliquam autem commodi enim harum nihil quasi, voluptates.</div>
+  <div className='app'>
+    <img src={excelIcon} alt=""/>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, quis saepe? Explicabo impedit numquam perferendis provident sed sit tempora ut vel veniam voluptas. Eos numquam perspiciatis possimus praesentium quasi quibusdam!</p>
+    <pre> {JSON.stringify(csvFile, null, 2)}</pre>
+  </div>
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,23 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
+const mode = process.env.NODE_ENV || 'development';
+
 module.exports = {
-  mode: "development",
+  mode: mode,
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: "[name].[hash].js",
+  },
+  devtool: process.env.NODE_ENV === 'production' ? false : 'source-map',
+  devServer: {
+    hot: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
   },
   module: {
     rules: [
@@ -17,13 +28,35 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"]
+            presets: ["@babel/preset-env", ["@babel/preset-react", {
+              runtime: "automatic"
+            }]]
           }
         }
       },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
+        use: 'file-loader'
+      },
+      {
+        test: /\.(ttf|woff|woff2)$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.xml$/,
+        use: 'xml-loader'
+      },
+      {
+        test: /\.csv$/,
+        use: 'csv-loader'
       },
     ]
   },
