525032db8479d0fa4e59b27bdeb2acab9280536b
YuraKolodiy13 <timber2017@gmail.com>
2021-09-13 15:01:58 +0300

    add copy-webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 
@@ -8,14 +9,17 @@ module.exports = {
   mode: mode,
   entry: "./src/index.js",
   output: {
+    assetModuleFilename: "images/[hash][ext][query]",
     path: path.resolve(__dirname, 'build'),
     filename: "[name].[hash].js",
+    chunkFilename: '[id].[chunkhash].js'
   },
   devtool: process.env.NODE_ENV === 'production' ? false : 'source-map',
   devServer: {
     hot: true,
   },
   resolve: {
+    extensions: ['.js', '.jsx'],
     alias: {
       '@': path.resolve(__dirname, 'src'),
     }
@@ -44,7 +48,7 @@ module.exports = {
       },
       {
         test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
-        use: 'file-loader'
+        type: "asset/resource",  //"asset", "asset/inline"
       },
       {
         test: /\.(ttf|woff|woff2)$/,
@@ -61,9 +65,21 @@ module.exports = {
     ]
   },
   plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public'),
+          globOptions: {
+            ignore: ["**/index.html"],
+          },
+          to: path.resolve(__dirname, 'build')
+        }
+      ]
+    }),
     new HtmlWebPackPlugin({
       template: "./public/index.html"
     }),
+
     new CleanWebpackPlugin()
   ]
 };
